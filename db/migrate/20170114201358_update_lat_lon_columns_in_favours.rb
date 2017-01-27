14d5c251994f51e683707ff267ac51f130eabe03
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-01-27 17:35:00 +0000

    Getting rid of bad migration

--- db/migrate/20170114201358_update_lat_lon_columns_in_favours.rb
+++
@@ -1,7 +0,0 @@
-class UpdateLatLonColumnsInFavours < ActiveRecord::Migration[5.0]
-  def change
-    change_column :favours, :latitude, :float
-    change_column :favours, :longitude, :float
-    add_column :favours, :address, :string
-  end
-end
--- db/migrate/20170127172456_remove_location_from_favours.rb
+++ db/migrate/20170127172456_remove_location_from_favours.rb
@@ -2,6 +2,5 @@ class RemoveLocationFromFavours < ActiveRecord::Migration[5.0]
   def change
     remove_column :favours, :latitude, :float
     remove_column :favours, :longitude, :float
-    remove_column :favours, :address, :string
   end
 end
