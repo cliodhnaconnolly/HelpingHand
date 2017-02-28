feb84fa1ba7522ad0481b211741f76dc04142fa8
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-02-28 02:44:20 +0000

    Trying to return to basic functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,12 @@ class StaticPagesController < ApplicationController
 
   def home
     @favour = current_user.favours.build if logged_in?
-    if(current_user.lat.nil?)
+    #if(current_user.lat.nil?)
       @favours = Favour.all
-    else
-      @favours = Favour.near([current_user.lat, current_user.long], 50)
-      @favours = @favours + Favour.where(longitude: nil)
-    end
+    #else
+    #  @favours = Favour.near([current_user.lat, current_user.long], 50)
+    #  @favours = @favours + Favour.where(longitude: nil)
+    #end
   end
 
   def help
