029c9d63492c9ac1b6d8c0ac0aa11efb1aef187c
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-03-03 18:10:18 +0000

    Mobile nil issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @favour = current_user.favours.build if logged_in?
-    if(current_user.lat.nil?)
+    if current_user.lat.blank? || current_user.lat.nil?
       @favours = Favour.all
     else
       @favours = Favour.near([current_user.lat, current_user.long], 50)
