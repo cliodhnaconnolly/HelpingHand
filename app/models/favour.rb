3846d87f3a41b035716522b9055932d52ac40add
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-03-06 21:22:17 +0000

    Validation for favours

--- app/models/favour.rb
+++ app/models/favour.rb
@@ -2,6 +2,8 @@ class Favour < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :user_id, presence: true
+  validates :title, presence: true
+  validates :deadline, presence: true
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode  # auto-fetch address
 end
