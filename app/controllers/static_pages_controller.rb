2ca671c68d95b21472d77070d82427c903b94aec
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-03-07 11:30:56 +0000

    Update homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,15 @@ class StaticPagesController < ApplicationController
 
   def home
     @favour = current_user.favours.build if logged_in?
-    if !logged_in? || current_user.lat.blank?
+    if(!logged_in? || current_user.lat.nil?)
       @favours = Favour.all
     else
       @favours = Favour.near([current_user.lat, current_user.long], 50)
       @favours = @favours + Favour.where(longitude: nil).reverse_order
-      @favours = @favours.find_all { |favour| favour.deadline.blank? || !favour.deadline.past?}
     end
+
+    @favours = @favours.paginate(page: params[:page])
+    @favours = @favours.find_all { |favour| favour.deadline.blank? || !favour.deadline.past?}
   end
 
   def help
