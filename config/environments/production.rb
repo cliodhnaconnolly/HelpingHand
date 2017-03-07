22e1bfe3ce4ce1b8c61f673c12e2138b19704b5d
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-03-07 01:28:21 +0000

    Further email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => 'cliodhna',
+      :user_name      => 'noreply',
       :password       => 'password',
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
