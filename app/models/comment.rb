dddeee08e1784745b3ef1028f9b8d53f74280a90
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-03-14 18:46:44 +0000

    Change ordering of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :favour
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   validates :user_id, presence: true
   #validates :favour_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
