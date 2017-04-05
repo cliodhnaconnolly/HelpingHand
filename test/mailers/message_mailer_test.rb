9cee38b9ecc591f3129430afe906f641955ec827
cliodhnaconnolly <cliodhnaconnolly@hotmail.com>
2017-04-05 23:26:46 +0100

    Test cases

--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class MessageMailerTest < ActionMailer::TestCase
   test "message_notification" do
-    mail = MessageMailer.message_notification
-    assert_equal "Message notification", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = MessageMailer.message_notification(conversations(:test_conversation), users(:connolly))
+    assert_equal "You've received a new message", mail.subject
+    assert_equal ["cliodhna@me.com"], mail.to
+    assert_equal ["from@helping-hand-ucd.herokuapp.com"], mail.from
+    assert_not_empty mail.body.encoded
   end
 
 end
